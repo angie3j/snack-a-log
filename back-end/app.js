8ca99f938367526a2c26ea1bfccbde91ec15eb41
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-11 16:00:40 -0500

    app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,19 @@
 // DEPENDENCIES
 
 const express = require("express");
+const cors = require("cors");
 
 // CONFIGURATION
 const app = express();
+app.use(cors());
 
 // MIDDLEWARE
+app.use(express.json())
 
 // ROUTES
+app.get('/', (_, response) => {
+    response.status(200).send('Get Snack\'n at Snack-a-log!')
+})
 
 // EXPORT
 module.exports = app;
