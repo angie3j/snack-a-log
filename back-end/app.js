c781fe1bd30a45e65a3b69efa33f1c995cdb6387
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-02-15 22:17:34 -0500

    gitignore file

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,6 @@
 // DEPENDENCIES
 const express = require("express");
 const cors = require("cors");
-
 const snackController = require("./controllers/snackController");
 
 // CONFIGURATION
@@ -10,19 +9,16 @@ const app = express();
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-
 app.use("/snacks", snackController);
 
 // ROUTES
-app.get("/", (_, res) => {
+app.get("/", (req, res) => {
   res.send("Get Snack'n at Snack-a-log!");
 });
 
 app.get("*", (_, res) => {
   res.status(404).json({ error: "Page not found" });
 });
-
-
 
 // EXPORT
 module.exports = app;
