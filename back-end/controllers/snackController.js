e8001f06c5a2a52b7496efe5ee24605275c8d47b
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-21 15:01:14 -0500

    Create CRUD Functions

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,40 +1,86 @@
 // 
 const express = require('express');
 const snacks = express.Router();
-
+const confirmHealth = require('../confirmHealth');
 
 const {
     getAllSnacks,
     getSnack,
     deleteSnack,
+    createSnack,
     updateSnack,
 } = require('../queries/snacks')
 
-// GET the entire snacks object
+// GET the entire Snacks object
 snacks.get('/', async (_, response) => {
     console.log('GET request to /snacks');
     const allSnacks = await getAllSnacks();
-    // Checking for an empty object
-    if (allSnacks.length === 0) {
-        response.status(404).json({error: 'Server Error'});
-    return;
-    }
-    response.status(200).json(allSnacks);
-});
+        response.status(200).json({ 
+            "success":true,
+            "payload": allSnacks,
+        });
+    })
 
-// GET a single snack by id
+// GET a single Snack by id
 snacks.get('/:id', async (request, response) => {
     console.log('GET request to /snacks/:id');
     const { id } = request.params;
     // function taking a parameter id
-    const snack = await getSnack(id);
-    // checking if snack is undefined
+    const oneSnack = await getSnack(id);
+    // checking if Snack is undefined
     if (snack === 'undefined') {
-        response.status(404).json({error: 'Server Error'});
+        response.status(404).json({
+            "success":false,
+            "payload": "item not found",
+            });
     return;
     }
-    response.status(200).json(snack);
+    response.status(200).json({
+        "success":false,
+        "payload": oneSnack,
+    });
 }); 
+
+// POST create a new snack - create
+snacks.post("/", async (req, res) => {
+    console.log("POST /snacks");
+    const { body } = req;
+    const createdSnack = await createSnack(req.body);
+    const finalObj = {};
+    if (createdSnack.id) {
+      finalObj.success = true;
+      finalObj.payload = createdSnack;
+    }
+    res.status(200).json(finalObj);
+  });
+
+//POST
+snacks.post('/:id', async (request, response) => {
+    console.log('POST /snacks');
+    const body = request
+    // checking if Snack is undefined
+    if (body.image === 'undefined') {
+        body.image = "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image"    
+        return 
+    }
+    body.name = body.name.split(' ').map(element => {
+        if(element.length > 2) {
+            return element[0].toUpperCase() + element.slice(1).toLowerCase()
+        }
+        return element
+    })
+    .join(' ')
+    body.is_healthy = confirmHealth(body)
+    
+    const snack = await createSnack(body)
+    if(!snack.name){
+       return res.status(422).json({error: "error"})
+    }
+    res.status(200).json({
+        "success":true,
+        "payload": snack,
+      })
+})
 
 // DELETE
 snacks.delete('/:id', async (request, response) => {
@@ -42,25 +88,16 @@ snacks.delete('/:id', async (request, response) => {
     const { id } = request.params;
     const deletedSnack = await deleteSnack(id);
     if (deletedSnack === 'undefined') {
-        response.status(404).json({error: 'Server Error'});
-    return
-    }
-    response.status(200).json(deletedSnack);
+        response.status(404).json({
+            "success":false,
+            "payload": "Cannot Delete",
+        });
+        return;
+    }    
+    response.status(200).json({
+        "success":true,
+        "payload": deletedSnack,
+    });
 }); 
 
-//POST
-
-snacks.put('/:id', async (request, response) => {
-    const updatedSnack = await updateSnack(request.body);
-    console.log(updatedSnack);
-    // checking if snack is undefined
-    if (updatedSnack === 'undefined') {
-        response.status(422).json({error: 'Must include name field'});
-    return
-    }
-    response.status(200).json(updatedSnack);
-}); 
-
-
-
-module.exports = snacks;
+module.exports = snacks
